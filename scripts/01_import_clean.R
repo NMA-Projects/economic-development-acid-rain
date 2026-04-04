922f79634b9dbc4403f2581e5f882730b08257f4
NMA-Projects <atkins339@googlemail.com>
2026-04-04 19:24:21 +0100

    Update 01_import_clean.R

--- scripts/01_import_clean.R
+++ scripts/01_import_clean.R
@@ -1,1 +1,7 @@
 
+# NOTE:
+# Country-level exclusions due to missing GDP or emissions data
+# will be implemented and documented in this script.
+# An explicit exclusion statement will be added to project documentation
+# once exclusions are finalised.
+
